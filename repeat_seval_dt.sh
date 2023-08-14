4c1d71d43cb873e87a3aa0d9cc37ef95ddbf318e
Dawid Motyka <dawidmt@gmail.com>
2023-08-14 13:15:41 +0200

    update seval script

--- repeat_seval_dt.sh
+++ repeat_seval_dt.sh
@@ -3,5 +3,6 @@
 for seed in {31546..31556}
 do
     echo "run_semeval with seed: $seed"
+    export PYTHONPATH=$PYTHONPATH:utils
     python -W ignore::UserWarning run_semeval.py --seed $seed --device 'cuda:0' --dataset 'dt'
 done
