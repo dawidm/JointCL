725968501b5c0921d5f25bcabdf896b7d709719c
Dawid Motyka <dawidmt@gmail.com>
2023-08-14 13:35:34 +0200

    add all seval scripts

---
+++ repeat_seval_cl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for seed in {31546..31556}
+do
+    echo "run_semeval with seed: $seed"
+    export PYTHONPATH=$PYTHONPATH:utils
+    python -W ignore::UserWarning run_semeval.py --seed $seed --device 'cuda:0' --dataset 'cc'
+done
