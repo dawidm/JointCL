2ad75fa04ade82aa967b0ec502d428facb998066
Dawid Motyka <dawidmt@gmail.com>
2023-08-16 12:15:48 +0200

    fix pythonpath

--- repeat_run_vast.sh
+++ repeat_run_vast.sh
@@ -3,5 +3,6 @@
 for seed in {31546..31556}
 do
     echo "run_vast with seed: $seed"
+    export PYTHONPATH=$PYTHONPATH:utils
     python -W ignore::UserWarning run_vast.py --seed $seed --device 'cuda:0' --dataset 'tradition'
 done
